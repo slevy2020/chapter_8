a5b328d68f33c387df90695db8d42bef5b0b140c
slevy2020 <slevy@chadwickschool.org>
2019-09-07 12:47:07 -0700

    feat: add door and controller

---
+++ Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour {
+  [SerializeField] private Vector3 dPos;
+
+  private bool _open;
+
+  public void Operate() {
+    if (_open) {
+      Vector3 pos = transform.position - dPos;
+      transform.position = pos;
+    } else {
+      Vector3 pos = transform.position + dPos;
+      transform.position = pos;
+    }
+    _open = !_open;
+  }
+}
