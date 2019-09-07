ea739cdcec047e785798cbf610334f5a1761171a
slevy2020 <slevy@chadwickschool.org>
2019-09-06 21:52:29 -0700

    feat: add jumping

--- Assets/Scripts/RelativeMovement.cs
+++ Assets/Scripts/RelativeMovement.cs
@@ -10,9 +10,19 @@ public class RelativeMovement : MonoBehaviour {
 
   public float moveSpeed = 6.0f;
 
+  public float jumpSpeed = 15.0f;
+  public float gravity = -9.8f;
+  public float terminalVelocity = -10.0f;
+  public float minFall = -1.5f;
+
+  private float _vertSpeed;
+
   private CharacterController _charController;
 
+  private ControllerColliderHit _contact;
+
   void Start() {
+    _vertSpeed = minFall;
     _charController = GetComponent<CharacterController>();
   }
 
@@ -35,7 +45,39 @@ public class RelativeMovement : MonoBehaviour {
       transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed*Time.deltaTime);
     }
 
+    bool hitGround = false;
+    RaycastHit hit;
+    if (_vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit)) {
+      float check = (_charController.height + _charController.radius) / 1.9f;
+      hitGround = hit.distance <= check;
+    }
+
+    if (hitGround) {
+      if (Input.GetButtonDown("Jump")) {
+        _vertSpeed = jumpSpeed;
+      } else {
+        _vertSpeed = minFall;
+      }
+    } else {
+      _vertSpeed += gravity*5*Time.deltaTime;
+      if (_vertSpeed < terminalVelocity) {
+        _vertSpeed = terminalVelocity;
+      }
+      if (_charController.isGrounded) {
+        if (Vector3.Dot(movement, _contact.normal) < 0) {
+          movement = _contact.normal*moveSpeed;
+        } else {
+          movement += _contact.normal*moveSpeed;
+        }
+      }
+    }
+    movement.y = _vertSpeed;
+
     movement *= Time.deltaTime;
     _charController.Move(movement);
+  }
+
+  void OnControllerColliderHit(ControllerColliderHit hit) {
+    _contact = hit;
   }
 }
