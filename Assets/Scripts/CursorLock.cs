be91cfe2bdbbcb374e4ccb4d48cbf8173e95532a
slevy2020 <slevy@chadwickschool.org>
2019-09-10 09:49:58 -0700

    feat: add cinemachine camera

---
+++ Assets/Scripts/CursorLock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour {
+  private Camera _camera;
+
+  void Start() {
+    _camera = GetComponent<Camera>();
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+  }
+}
