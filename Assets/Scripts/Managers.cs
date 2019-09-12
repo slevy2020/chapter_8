992d9aa6ee8cf8fd07f3ceca25f365ac4e0085f5
slevy2020 <slevy@chadwickschool.org>
2019-09-12 08:48:50 -0700

    feat: add startup scene

--- Assets/Scripts/Managers.cs
+++ Assets/Scripts/Managers.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 [RequireComponent(typeof(PlayerManager))]
 [RequireComponent(typeof(InventoryManager))]
+[RequireComponent(typeof(MissionManager))]
 
 public class Managers : MonoBehaviour {
   public static PlayerManager Player {get; private set;}
   public static InventoryManager Inventory {get; private set;}
+  public static MissionManager Mission {get; private set;}
 
   private List<IGameManager> _startSequence;
 
   void Awake() {
+    DontDestroyOnLoad(gameObject);
+
     Player = GetComponent<PlayerManager>();
     Inventory = GetComponent<InventoryManager>();
+    Mission = GetComponent<MissionManager>();
 
     _startSequence = new List<IGameManager>();
     _startSequence.Add(Player);
     _startSequence.Add(Inventory);
+    _startSequence.Add(Mission);
 
     StartCoroutine(StartupManagers());
   }
@@ -46,11 +52,13 @@ public class Managers : MonoBehaviour {
 
       if (numReady > lastReady) {
         Debug.Log("Progress: " + numReady + "/" + numModules);
+        Messenger<int, int>.Broadcast(StartupEvent.MANAGERS_PROGRESS, numReady, numModules);
       }
 
       yield return null;
     }
 
     Debug.Log("All managers started up");
+    Messenger.Broadcast(StartupEvent.MANAGERS_STARTED);
   }
 }
